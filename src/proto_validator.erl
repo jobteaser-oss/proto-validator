36d1c3c3dc8ddfa3e0baf83c4e496247a2054c1f
Nicolas Martyanoff <khaelin@gmail.com>
2020-01-13 11:38:27 +0100

    implement error reporting and exit status

--- src/proto_validator.erl
+++ src/proto_validator.erl
@@ -21,6 +21,9 @@
                 | {include, string()}
                 | verbose.
 
+-type validation_result() :: {file:name_all(), ok | {error, term()}}.
+-type validation_results() :: list(validation_result()).
+
 -spec main(list(string())) -> no_return().
 main(Args) ->
   %% Global options
@@ -29,8 +32,6 @@ main(Args) ->
   CommandLineSpec = command_line_spec(),
   case getopt:parse(CommandLineSpec, Args) of
     {ok, {Options, ProtoFiles}} ->
-      %% Verbosity
-      put(verbose, proplists:is_defined(verbose, Options)),
       %% Help string
       Help = proplists:is_defined(help, Options),
       if
@@ -39,32 +40,54 @@ main(Args) ->
           erlang:halt(0);
         true -> ok
       end,
-      %% Configuration file
-      Config = case load_config(Options) of
-                 {ok, Config2} ->
-                   Config2;
-                 {error, ConfigErrReason} ->
-                   die("cannot load configuration: ~p", [ConfigErrReason])
-               end,
-      %% File validation
-      Results = validate_proto_files(ProtoFiles, Config, Options),
-      info("results: ~p", [Results]); % TODO formatting
+      %% Main command
+      main_validate(Options, ProtoFiles);
     {error, {Reason, Data}} ->
-      io:format(standard_error, "error: ~s ~p~n~n", [Reason, Data]),
+      log_error("~s ~p", [Reason, Data]),
       getopt:usage(CommandLineSpec, escript:script_name()),
       erlang:halt(1)
   end.
 
--spec info(string(), list(term())) -> ok.
-info(Format, Args) ->
-  case get(verbose) of
-    true -> io:format(Format ++ "\n", Args), ok;
-    _ -> ok
+-spec main_validate(options(), Files) -> ok when
+    Files :: list(file:name_all()).
+main_validate(Options, Files) ->
+  %% Verbosity
+  put(verbose, proplists:is_defined(verbose, Options)),
+  %% Configuration file
+  Config = case load_config(Options) of
+             {ok, Config2} ->
+               Config2;
+             {error, ConfigErrReason} ->
+               die("cannot load configuration: ~p", [ConfigErrReason])
+           end,
+  %% File validation
+  Results = validate_proto_files(Files, Config, Options),
+  lists:foreach(fun process_validation_result/1, Results),
+  case are_validation_results_success(Results) of
+    true ->
+      ok;
+    false ->
+      erlang:halt(1)
   end.
+
+-spec log_info(string(), list(term())) -> ok.
+log_info(Format, Args) ->
+  case get(verbose) of
+    true ->
+      io:format(Format ++ "\n", Args),
+      ok;
+    _ ->
+      ok
+  end.
+
+-spec log_error(string(), list(term())) -> ok.
+log_error(Format, Args) ->
+  io:format(standard_error, Format ++ "\n", Args),
+  ok.
 
 -spec die(string(), list(term())) -> no_return().
 die(Format, Args) ->
-  io:format(standard_error, Format ++ "\n", Args),
+  log_error(Format ++ "\n", Args),
   erlang:halt(1).
 
 -spec command_line_spec() -> list(getopt:option_spec()).
@@ -86,14 +109,10 @@ load_config(Options) ->
   end.
 
 -spec validate_proto_files(Files, Config, options()) ->
-        list({File, Result}) when
-    Files :: list(File),
-    File :: file:name_all(),
-    Config :: proto_validator_config:config(),
-    Result :: ok | {error, term()}.
+        validation_results() when
+    Files :: list(file:name_all()),
+    Config :: proto_validator_config:config().
 validate_proto_files(Files, Config, Options) ->
-  info("using options ~p", [Options]),
-  info("validating proto files ~p", [Files]),
   IncludePaths = proplists:get_all_values(include_path, Options),
   GpbBaseOpts = [use_packages,
                  to_proto_defs,
@@ -110,21 +129,21 @@ validate_proto_files(Files, Config, Options) ->
     Config :: proto_validator_config:config(),
     Reason :: term().
 validate_proto_file(File, Config, _Options, GpbOptions) ->
-  info("validating proto file ~p", [File]),
   case gpb_compile:file(File, GpbOptions) of
     {ok, Definitions} ->
       validate_definitions(Definitions, Config);
     {ok, Definitions, Warnings} ->
       lists:foreach(fun (Warning) ->
-                        info("warning: ~p", [Warning])
+                        log_info("warning: ~p", [Warning])
                     end, Warnings),
       validate_definitions(Definitions, Config);
     {error, Reason} ->
       {error, {compile_error, Reason}}
   end.
 
--spec validate_definitions(gpb_defs:defs(), Config) -> ok | {error, reason} when
-    Config :: proto_validator_config:config().
+-spec validate_definitions(gpb_defs:defs(), Config) -> ok | {error, Reason} when
+    Config :: proto_validator_config:config(),
+    Reason :: term().
 validate_definitions(Definitions, Config) ->
   Catalog = proto_validator_catalog:catalog(Definitions),
   ObjectsData = proto_validator_rules:collect_objects_data(Catalog),
@@ -143,3 +162,34 @@ validate_definitions(Definitions, Config) ->
     Errors ->
       {error, Errors}
   end.
+
+-spec are_validation_results_success(validation_results()) -> boolean().
+are_validation_results_success(Results) ->
+  lists:all(fun ({_, Result}) -> Result =:= ok end, Results).
+
+-spec process_validation_result(validation_result()) -> ok.
+process_validation_result({File, Result}) ->
+  case Result of
+    ok ->
+      ok;
+    {error, Reasons} when is_list(Reasons) ->
+      lists:foreach(fun (Reason) ->
+                        report_validation_error(File, Reason)
+                    end, Reasons),
+      ok;
+    {error, Reason} ->
+      report_validation_error(File, Reason),
+      ok
+  end.
+
+-spec report_validation_error(file:name_all(), term()) -> ok.
+report_validation_error(File, Reason) ->
+  ReasonString = format_error_reason(Reason),
+  log_error("~s: ~s", [File, ReasonString]).
+
+-spec format_error_reason(term()) -> iolist().
+format_error_reason({test_failure, _Test, {Object, Data}, Message}) ->
+  ObjectName = maps:get(name, Data),
+  io_lib:format("invalid ~p ~p: ~s", [Object, ObjectName, Message]);
+format_error_reason(Reason) ->
+  io_lib:format("~p", [Reason]).
